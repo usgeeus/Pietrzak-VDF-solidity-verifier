cdf8dcf6f8ec5340e3d5658a4f929d6a68155bb9
지의신 <64007655+usgeeus@users.noreply.github.com>
2024-08-11 22:22:08 +0900

    create Minimal application

---
+++ src/MinimalPietrzak.sol
@@ -0,0 +1,97 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+import "./libraries/WesolowskiLibrary.sol";
+import "./libraries/PietrzakLibrary.sol";
+
+interface IMinimalPietrzak {
+    function verifyPietrzak(
+        BigNumber[] memory v,
+        BigNumber memory x,
+        BigNumber memory y,
+        BigNumber memory n,
+        uint256 delta,
+        uint256 T
+    ) external view returns (bool);
+}
+
+contract MinimalPietrzakHalving {
+    function verifyPietrzak(
+        BigNumber[] memory v,
+        BigNumber memory x,
+        BigNumber memory y,
+        BigNumber memory n,
+        uint256 delta,
+        uint256 T
+    ) external returns (bool) {
+        return PietrzakLibraryMeasureHalvingGas.verify(v, x, y, n, delta, T);
+    }
+}
+
+contract MinimalPietrzakModExp {
+    function verifyPietrzak(
+        BigNumber[] memory v,
+        BigNumber memory x,
+        BigNumber memory y,
+        BigNumber memory n,
+        uint256 delta,
+        uint256 T
+    ) external returns (bool) {
+        return PietrzakLibraryMeasureModExpGas.verify(v, x, y, n, delta, T);
+    }
+}
+
+contract MinimalPietrzakHalvingReturnGas {
+    function verifyPietrzak(
+        BigNumber[] memory v,
+        BigNumber memory x,
+        BigNumber memory y,
+        BigNumber memory n,
+        uint256 delta,
+        uint256 T
+    ) external view returns (uint256) {
+        return
+            PietrzakLibraryMeasureHalvingGas.verifyReturnGas(
+                v,
+                x,
+                y,
+                n,
+                delta,
+                T
+            );
+    }
+}
+
+contract MinimalPietrzakModExpReturnGas {
+    function verifyPietrzak(
+        BigNumber[] memory v,
+        BigNumber memory x,
+        BigNumber memory y,
+        BigNumber memory n,
+        uint256 delta,
+        uint256 T
+    ) external view returns (uint256) {
+        return
+            PietrzakLibraryMeasureModExpGas.verifyReturnGas(
+                v,
+                x,
+                y,
+                n,
+                delta,
+                T
+            );
+    }
+}
+
+contract MinimalPietrzak {
+    function verifyPietrzak(
+        BigNumber[] memory v,
+        BigNumber memory x,
+        BigNumber memory y,
+        BigNumber memory n,
+        uint256 delta,
+        uint256 T
+    ) external view returns (bool) {
+        return PietrzakLibrary.verify(v, x, y, n, delta, T);
+    }
+}
